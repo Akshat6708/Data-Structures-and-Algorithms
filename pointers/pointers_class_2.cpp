d22c5f7d485141f85feaee21e35896d48e32cb1b
akshat patidar <akshatpatidar29@gmail.com>
2025-04-25 15:03:13 +0530

    add pointers class 2

---
+++ pointers/pointers_class_2.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std; 
+int main(){
+    // int arr[4]={10,20,30,40};
+//    cout<<arr<<endl;
+//    cout<<&arr<<endl;
+//    cout<<&arr[0]<<endl;
+
+
+//    int *p=arr;
+//    cout<<p<<endl;
+//    cout<<&p<<endl;
+//    cout<<*p<<endl;
+
+
+int arr[4]={10,20,30,40};
+// cout<<*arr<<endl;
+// cout<<arr[0]<<endl;
+// cout<<*arr+1<<endl;
+// cout<<*(arr)+1<<endl;
+// cout<<*(arr+1)<<endl;
+// cout<<arr[1]<<endl;
+// cout<<*(arr+2)<<endl;
+// cout<<arr[2]<<endl;
+// cout<<*(arr+3)<<endl;
+// cout<<arr[3]<<endl;
+
+
+// cout<<0[arr]<<endl;
+// for(int i=0; i<4; i++){
+//     cout<<i[arr]<<endl;
+// }
+
+
+    
+//   poiners in character array
+
+char ch[40] ="akshatpatidar";
+char*chpt=&ch[0];
+cout<<ch<<endl;
+cout<<&ch<<endl;
+cout<<&ch[0]<<endl;
+cout<<*(ch+3)<<endl;
+cout<<ch[0]<<endl;
+cout<<chpt<<endl;
+cout<<chpt+2<<endl;
+cout<<*chpt<<endl;
+
+    return 0;
+
+}
