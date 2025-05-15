784363f6289daa3b02db43183d4d35a6033d90aa
akshat patidar <akshatpatidar29@gmail.com>
2025-05-15 18:28:25 +0530

    add some file

--- Array_Questions/Q_9_Left_rotated_arrary_by_D_places.cpp
+++ Array_Questions/Q_9_Left_rotated_arrary_by_D_places.cpp
@@ -1,32 +1,47 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int main(){
-    int d;
-    cout<<"enter the value of d : ";
-    cin>>d;
+// int main(){
+//     int d;
+//     cout<<"enter the value of d : ";
+//     cin>>d;
   
-    vector<int > arr{1,2,3,4,5,6};
-    int n= arr.size();
-      d=d%n;
-    int temp[d];
-    // store the value in the temp arr
-    for(int i=0; i<d; i++){
-       temp[i]=arr[i];
+//     vector<int > arr{1,2,3,4,5,6};
+//     int n= arr.size();
+//       d=d%n;
+//     int temp[d];
+//     // store the value in the temp arr
+//     for(int i=0; i<d; i++){
+//        temp[i]=arr[i];
+//     }
+
+//     // now place the remainig element by d place
+
+//     for(int i=d; i<n; i++){
+//       arr[i-d]=arr[i];
+//     }
+//   // now push the value temp to arr
+
+//   for(int i=n-d; i<n;i++){
+//     arr[i]=temp[i-(n-d)];
+//   }
+
+//   for(auto i:arr){
+//     cout<<i<<" ";
+//   }
+//     }
+
+
+
+
+int main(){
+    vector<int > nums{1,2,3,4,5};
+    vector<string>ans;
+    for(int i=0 ; i<nums.size(); i++){
+        string s=to_string(nums[i]);
+        ans.push_back(s);
     }
-
-    // now place the remainig element by d place
-
-    for(int i=d; i<n; i++){
-      arr[i-d]=arr[i];
+    for(auto i:ans){
+                cout<<i<<" ";
     }
-  // now push the value temp to arr
-
-  for(int i=n-d; i<n;i++){
-    arr[i]=temp[i-(n-d)];
-  }
-
-  for(auto i:arr){
-    cout<<i<<" ";
-  }
-    }
+}
---
+++ Backtracking/Q_Print_all_Permutation.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void printPermutation(string &str , int i){
+    // base case 
+    if(i>=str.length()) {
+        cout<<str<<" ";
+        return;
+    }
+    // swaping
+    for(int j=i; j<str.length(); j++){
+        swap(str[i],str[j]);
+         // call recursion 
+    printPermutation(str, i+1);
+         // backtracking 
+        swap(str[i],str[j]);
+    }
+}
+int main(){
+    string str="abc";
+    int i=0;
+    printPermutation(str, i);
+    return 0;
+}
