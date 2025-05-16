bf34be7a120270205c27b90ab4fe8a29f5949177
akshat patidar <akshatpatidar29@gmail.com>
2025-05-16 16:32:14 +0530

    add  Q_Print_all_Permutation.cpp or  Q_Rat_in_a_Maze.cpp

--- Backtracking/Q_Print_all_Permutation.cpp
+++ Backtracking/Q_Print_all_Permutation.cpp
@@ -1,24 +1,48 @@
 #include<iostream>
 using namespace std;
 
-void printPermutation(string &str , int i){
+// void printPermutation(string &str , int i){
+//     // base case 
+//     if(i>=str.length()) {
+//         cout<<str<<" ";
+//         return;
+//     }
+//     // swaping
+//     for(int j=i; j<str.length(); j++){
+//         swap(str[i],str[j]);
+//          // call recursion 
+//     printPermutation(str, i+1);
+//          // backtracking 
+//         swap(str[i],str[j]);
+//     }
+// }
+// int main(){
+//     string str="abc";
+//     int i=0;
+//     printPermutation(str, i);
+//     return 0;
+// }
+
+void printPremutation(string& str, int i){
     // base case 
-    if(i>=str.length()) {
-        cout<<str<<" ";
-        return;
+    if(i>=str.length()){
+    cout<<str<<" ";
+       return ;
     }
-    // swaping
-    for(int j=i; j<str.length(); j++){
-        swap(str[i],str[j]);
-         // call recursion 
-    printPermutation(str, i+1);
-         // backtracking 
-        swap(str[i],str[j]);
-    }
+    // recursion call
+  
+    for(int j=i; j<str.length();j++){
+        swap(str[i], str[j]);
+          // recursion call
+       printPremutation(str, i+1); 
+       // backtraking
+        swap(str[i], str[j]);
+    }  
 }
+ 
+
 int main(){
-    string str="abc";
+    string str= "abc";
     int i=0;
-    printPermutation(str, i);
-    return 0;
+    printPremutation(str, i);
 }
